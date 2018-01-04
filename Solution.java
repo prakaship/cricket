2dbd1293987225e45ca78b7f8e68839efcf2ba54
prakaship <34982386+prakaship@users.noreply.github.com>
2018-01-05 02:22:49 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -2,25 +2,25 @@ import java.util.*;
  class Game
 {
 	public HashMap<Integer,String> countries=new HashMap<Integer,String>();
-	Game()
-	{
-		 countries.put(1,"India");
+	Game(){
+	
+	 countries.put(1,"India");
          countries.put(2,"Australia");
          countries.put(3,"South Africa");
          countries.put(4,"New Zealand");
          countries.put(5,"West Indies");
          countries.put(6,"Srilanka");
 	}
-    public void displayCountry()             // for displaying the country list
-	{
-		Set s=countries.entrySet();
+
+    public void displayCountry() {                                           // for displaying the country list
+            
+	   	Set s=countries.entrySet();
 		Iterator i=s.iterator();
-		while(i.hasNext())
-		{
+		while(i.hasNext()){
+
 			Map.Entry country = (Map.Entry)i.next();
 			System.out.println(country.getKey()+" "+country.getValue());
-			
-        
+		       
 		}
 			
 	}
@@ -34,7 +34,6 @@ public class Solution {
 		Scanner sc=new Scanner(System.in);
 		Game c=new Game();
 		c.displayCountry();
-		
-		
+	
 	}
 }
