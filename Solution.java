2c65b90f1ce6dd33ea3760324ce7b063df771fdf
prakaship <34982386+prakaship@users.noreply.github.com>
2018-01-05 04:55:10 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,39 +1,96 @@
+
 import java.util.*;
+import java.io.*;
  class Game
 {
-	public HashMap<Integer,String> countries=new HashMap<Integer,String>();
-	Game(){
-	
-	 countries.put(1,"India");
+	 public  HashMap<Integer,String> countries=new HashMap<Integer,String>();
+	 String players[][]= {{"Rohit Sharma","Shikar Dhawan","Virat Kohli(c)","Manish Pandey","MS Dhoni","Hardik Pandya","Dinesh Karthik","Axar Patel","Jasprit Bumrah","Bhuvaneshwar Kumar","Mohammed Shami"},
+			 {"David Warner","Aaron Finch","Steven Smith(c)","Glenn Maxwell","Travis Head","Marcus Stoinis","Peter Handscomb","Ashton Agar","Pat Cummins","Nathan Coulter-Nile","Kane Richardson"},
+			 {"Faf du Plessis(c)", "Kyle Abbott", "Hashim Amla", "Quinton de Kock", "Jean-Paul Duminy", "Imran Tahir", "David Miller", "Wayne Parnell", "Kagiso Rabada", "Rilee Rossouw", "Dale Steyn"},
+	         {"Jason Roy","Jonny Bairstow","Joe Root(c)","Eoin Morgan","Jos Buttler","Sam Billings","Moeen Ali","Liam Plunkett","Adil Rashid","Jake Ball", "Tom Curran"},
+	         {"Jason Holder(c)","Jason Mohammed", "Shannon Gabriel", "Chris Gayle", "Kyle Hope", "Shai Hope", "Evin Lewis", "Nikita Miller","Rovman Powell", "Kesrick Williams", "Sheldon Cottrell"},
+	         {"Danushka Gunathilaka","Upul Tharanga","Lahiru Thirimanne","Angelo Mathews(c)","Niroshan Dickwella","Asela Gunaratne","Thisara Perera","Sachith Pathirana","Suranga Lakmal","Akila Dananjaya","Nuwan Pradeep"},
+	 };
+	 String bowlers[][]= { {"Hardik Pandya","Axar Patel","Jasprit Bumrah","Bhuvaneshwar Kumar","Mohammed Shami"},
+			 {"Marcus Stoinis","Ashton Agar","Pat Cummins"," Nathan Coulter-Nile","Kane Richardson"},	
+			 {"Imran Tahir","Wayne Parnell", "Kagiso Rabada", "Rilee Rossouw", "Dale Steyn"},
+			 {"Moeen Ali","Liam Plunkett","Adil Rashid","Jake Ball","Tom Curran"},
+			 {"Jason Holder","Evin Lewis","Rovman Powell", "Kesrick Williams", "Sheldon Cottrell"},
+			 {"Angelo Mathews","Sachith Pathirana","Suranga Lakmal","Akila Dananjaya","Nuwan Pradeep"}
+			
+			 };
+	public static  String line="-------------------------------";
+	void  displayPlayers(int team)
+	 {  
+		System.out.println(countries.get(team)+"  players ");
+		System.out.println(line);
+		 for(int i=0;i<11;i++)
+			 System.out.println(players[team-1][i]);
+	 }
+			 
+			 
+ Game()
+	{
+		 countries.put(1,"India");
          countries.put(2,"Australia");
          countries.put(3,"South Africa");
-         countries.put(4,"New Zealand");
+         countries.put(4,"England");
          countries.put(5,"West Indies");
          countries.put(6,"Srilanka");
 	}
-
-    public void displayCountry() {                                           // for displaying the country list
-            
-	   	Set s=countries.entrySet();
+    public void displayCountry()             // for displaying the country list
+	{
+    	System.out.println("choose any two countrys to start the match");
+		Set s=countries.entrySet();
 		Iterator i=s.iterator();
-		while(i.hasNext()){
-
+		while(i.hasNext())
+		{
 			Map.Entry country = (Map.Entry)i.next();
 			System.out.println(country.getKey()+" "+country.getValue());
-		       
+			      
 		}
-			
+		System.out.println("choose any two countrys to start the match");	
+	
 	}
 }
 
 
+
+
+
+
 public class Solution {
-	public static void main(String [] args) {
+	
+	public static void main(String [] args) throws Exception{
 		
 		System.out.println("welcome to the game of cricket");
-		Scanner sc=new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Game c=new Game();
 		c.displayCountry();
-	
+		int team_1;
+		int team_2;
+		while(true)
+		{
+			team_1=Integer.parseInt(br.readLine());
+			team_2=Integer.parseInt(br.readLine());
+			
+			if(team_1!=team_2 && team_1<=c.countries.size()  && team_2<=c.countries.size()) {
+				System.out.println("enter the number of overs");
+				break;
+			}
+			
+			else if(team_1==team_2)
+				System.out.println("enter the differnt countries");
+			
+			else if(team_1>c.countries.size() ||team_2>c.countries.size())
+				System.out.println("enter a valid country number");
+		}
+		int overs=Integer.parseInt(br.readLine());
+		
+		c.displayPlayers(team_1);
+		System.out.println("\n \n");
+		Thread.sleep(500);
+		c.displayPlayers(team_2);
+		
 	}
 }
